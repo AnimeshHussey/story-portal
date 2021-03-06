24be7a8f1264325e1c6cdefeac16d9ac3a1247b0
AnimeshHussey <kpsmiling92@gmail.com>
2021-03-06 12:25:38 +0530

    Update modal.js

--- src/component/common/modal.js
+++ src/component/common/modal.js
@@ -10,7 +10,7 @@ function ModalCreateStory({ open, setOpen }) {
       onOpen={() => setOpen(true)}
       open={open}
       size="small"
-      trigger={<Button>Basic Modal</Button>}
+      trigger={<Button>Create Story</Button>}
     >
       <Header>Create Story</Header>
       <Modal.Content>
